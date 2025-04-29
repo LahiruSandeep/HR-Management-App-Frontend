1aac9e27994c9d337f96ed86a6cc5d1332e9f5f6
Lahiru Sandeepa <lahirusandeepa9999@gmai.com>
2025-04-29 13:06:53 +0530

    add components{header,dash-page,sign-in}

---
+++ src/app/pages/dash-page/dash-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dash-page',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './dash-page.component.html',
+  styleUrl: './dash-page.component.css'
+})
+export class DashPageComponent {
+
+}
